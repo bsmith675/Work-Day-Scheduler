a3216780159214db3d61499f8c73698805357538
Ben Smith <benlsmith329@gmail.com>
2022-04-05 17:10:20 -0400

    finished array on line 28

--- script.js
+++ script.js
@@ -16,4 +16,14 @@ for (time = 9; time <= 17; time++) {
         ampm = "am";
         dHour = time;
     }
+    dHour = dHour.toString();
+
+    dataPlanner = {
+        id: id,
+        dHour: dHour,
+        time: time,
+        ampm: ampm,
+        dataPlanner: dataPlanner,
+    };
+    workDayPlanner.push(dataPlanner);
 }
