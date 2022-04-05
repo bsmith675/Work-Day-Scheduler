fdbfc524e063ca19644011d2ae55bb532f902498
Ben Smith <benlsmith329@gmail.com>
2022-04-05 17:18:22 -0400

    added data display

--- script.js
+++ script.js
@@ -31,3 +31,9 @@ function currentDate() {
     var cDate = moment().format("dddd, MMMM D0");
     $("#currentDay").text(cDate);
 }
+
+function plannerDataDisplay() {
+    workDayPlanner.forEach(function (hour) {
+        $("#" + hour.id).val(hour.dataPlanner);
+    });
+}
